2fe1091f839fb4f6c7df906d361c9086ddc91475
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-08-22 13:06:26 +0300

    add nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { MfowAppComponent } from './mflow-app.component';
 import { CommentsComponent } from './comments/comments.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     MfowAppComponent,
-    CommentsComponent
+    CommentsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
