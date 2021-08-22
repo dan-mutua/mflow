3e0cbe3d359e937033a2f77894b6b96101aaaa07
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-08-22 12:53:29 +0300

    add quotes and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MfowAppComponent } from './mflow-app.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
-    MfowAppComponent
+    MfowAppComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [MfowAppComponent]
+  bootstrap: [MfowAppComponent, CommentsComponent]
 })
 export class AppModule { }
