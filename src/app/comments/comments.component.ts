f8031d9b1f94cc019b8b0f045cc66aff26d0d585
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-08-23 01:00:44 +0300

    add number of likes and dislikes

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -3,22 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.css']
+  styleUrls: ['./comments.component.css'],
 })
 export class CommentsComponent implements OnInit {
-    title= 'comment'
-  likes: number=0;
-  dislikes: number=0;
+  title = 'comment';
+  likes: number = 0;
+  dislikes: number = 0;
+  likes2: number = 0;
+  dislikes2: number = 0;
 
-  likeButtonClick(){
+  likesButtonClick() {
     this.likes++;
   }
 
-  dislikeButtonClick(){
-   this.dislikes++;
+  dislikeButtonClick() {
+    this.dislikes++;
   }
 
-  ngOnInit(): void {
+  likesButtonClick1() {
+    this.likes++;
   }
 
+  dislikeButtonClick1() {
+    this.dislikes++;
+  }
+
+  ngOnInit(): void {}
 }
