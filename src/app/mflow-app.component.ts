edcc95c38d00eeba7b825281acb935d71bbea653
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-08-22 14:04:29 +0300

    correct a compile error

--- src/app/mflow-app.component.ts
+++ src/app/mflow-app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mflow-app-root',
-  template: '<h2>welcome to mflow</h2>',
+  template : '<h2>welcome to mflow</h2>',
   
 })
 export class MfowAppComponent {
