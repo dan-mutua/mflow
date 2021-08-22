12c9367c1571a16817625be94521614a57eb9f27
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-08-22 11:05:54 +0300

    add the nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MfowAppComponent } from './mflow-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MfowAppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MfowAppComponent]
 })
 export class AppModule { }
---
+++ src/app/mflow-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mflow-app-root',
+  template: '<h2>welcome to mflow</h2>',
+  
+})
+export class MfowAppComponent {
+  title = 'mflow';
+}
